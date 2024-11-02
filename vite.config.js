528d377168f13af6556c78325171ca98ceb81fa9
MDanielle007 <marc.danielle.cabatay219@gmail.com>
2024-11-02 10:25:52 +0800

    pwa commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
-import {PrimeVueResolver} from 'unplugin-vue-components/resolvers';
+import { PrimeVueResolver } from 'unplugin-vue-components/resolvers';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,34 @@ export default defineConfig({
       resolvers: [
         PrimeVueResolver()
       ]
+    }),
+    VitePWA({
+      registerType: 'autoUpdate', 
+      devOptions:{
+        enabled: true,
+      },
+      manifest: {
+        lang: 'en-US',
+        name: 'Simple Notepad App',
+        short_name: 'SNA',
+        description: 'Simple Notepad App using VueJS',
+        theme_color: '#F59E0B',
+        background_color: '#F59E0B',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
     })
   ],
 })
